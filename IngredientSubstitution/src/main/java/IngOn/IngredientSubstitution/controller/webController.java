ee0008443f01683767f84ff7c5e6896df3534f1f
runchana <runchana.see@outlook.com>
2023-10-24 14:26:18 +0700

    update cooking type

--- IngredientSubstitution/src/main/java/IngOn/IngredientSubstitution/controller/webController.java
+++ IngredientSubstitution/src/main/java/IngOn/IngredientSubstitution/controller/webController.java
@@ -1,7 +1,9 @@
 package IngOn.IngredientSubstitution.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class webController {
 
     @GetMapping("/")
---
+++ IngredientSubstitution/src/main/java/IngOn/IngredientSubstitution/service/OntologyService.java
@@ -0,0 +1,14 @@
+package IngOn.IngredientSubstitution.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OntologyService {
+
+    @Autowired
+    Environment env;
+
+    String ontologyPath =
+}
