ab6197f9a8384da8685f9b79407781c2956f5a6e
runchana <runchana.see@outlook.com>
2023-11-06 19:04:21 +0700

    update concept list and navbar

---
+++ IngredientSubstitution/src/main/java/IngOn/IngredientSubstitution/controller/WebController.java
@@ -0,0 +1,39 @@
+package IngOn.IngredientSubstitution.controller;
+
+import IngOn.IngredientSubstitution.service.OntologyService;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
+import java.util.List;
+
+@Controller
+public class WebController {
+
+    private static final File owlFile = new File("IngredientSubstitution/src/main/resources/ontology/ThaiIngredients-v4.owl");
+
+    @GetMapping("/")
+    public String homePage() {
+        return "index";
+    }
+
+    @GetMapping("/aboutus")
+    public String aboutUs() { return "aboutUs"; }
+
+    @GetMapping("/ingredient")
+    public String ingredient (@RequestParam("id") String selectedId, Model model) throws OWLOntologyCreationException {
+        OWLOntology ontology = OntologyService.prepareOWLFile(owlFile);
+
+        List<String> conceptList = OntologyService.retrieveConceptName(ontology, selectedId);
+
+        String formattedConceptList = String.join("\n", conceptList);
+
+        model.addAttribute("conceptList", formattedConceptList);
+
+        return "ingredient";
+    }
+}
--- IngredientSubstitution/src/main/java/IngOn/IngredientSubstitution/service/OntologyService.java
+++ IngredientSubstitution/src/main/java/IngOn/IngredientSubstitution/service/OntologyService.java
@@ -4,12 +4,81 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.util.ShortFormProvider;
+import org.semanticweb.owlapi.util.SimpleShortFormProvider;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Service
 public class OntologyService {
-
     @Autowired
     Environment env;
 
+    private static Set<String> processedProperties = new HashSet<>();
 
+    public static OWLOntology prepareOWLFile(File owlFile) {
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        OWLOntology ontology = null;
+        try {
+            ontology = manager.loadOntologyFromOntologyDocument(IRI.create(owlFile));
+        } catch (OWLOntologyCreationException e) {
+            e.printStackTrace();
+        }
 
+        return ontology;
+    }
+
+    public static List<String> retrieveConceptName(OWLOntology ontology, String foodGroup) throws OWLOntologyCreationException {
+        List<String> conceptNames = new ArrayList<>();
+
+        OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
+        OWLReasoner reasoner = reasonerFactory.createReasoner(ontology);
+
+        OWLClass cerealClass = getOWLClassByName(ontology, foodGroup);
+
+        for (OWLClass cls : ontology.getClassesInSignature()) {
+            if (reasoner.getSubClasses(cerealClass, true).containsEntity(cls)) {
+                String className = getShortForm(cls, ontology);
+                conceptNames.add(className);
+            }
+        }
+
+        return conceptNames;
+    }
+
+    public static OWLClass getOWLClassByName(OWLOntology ontology, String className) {
+        for (OWLClass cls : ontology.getClassesInSignature()) {
+            if (getShortForm(cls, ontology).equals(className)) {
+                return cls;
+            }
+        }
+        return null;
+    }
+
+    public static String getShortForm(OWLClass cls, OWLOntology ontology) {
+        ShortFormProvider shortFormProvider = new SimpleShortFormProvider();
+        return shortFormProvider.getShortForm(cls);
+    }
+
+    public static void addObjectProperties(OWLOntology ontology, OWLClass owlClass, ShortFormProvider shortFormProvider) {
+
+        for (OWLIndividual individual : owlClass.getIndividuals(ontology)) {
+            for (OWLObjectPropertyAssertionAxiom axiom : ontology.getObjectPropertyAssertionAxioms(individual)) {
+                OWLObjectProperty property = axiom.getProperty().asOWLObjectProperty();
+
+                String propertyName = shortFormProvider.getShortForm(property);
+
+                processedProperties.add(propertyName);
+            }
+        }
+    }
 }
