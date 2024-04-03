fde0f15de1878b0ac4d72ad78c257af9dd17b939
runchana <runchana.see@outlook.com>
2024-04-03 22:56:21 +0700

    retrieve properties

--- IngredientSubstitution/src/main/java/IngOn/IngredientSubstitution/service/DescriptionLogicDisplayService.java
+++ IngredientSubstitution/src/main/java/IngOn/IngredientSubstitution/service/DescriptionLogicDisplayService.java
@@ -1,19 +1,13 @@
 package IngOn.IngredientSubstitution.service;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class DescriptionLogicDisplayService {
 
-    private static HashMap<String, HashMap<String, Set<String>>> conceptList = new HashMap<>();
-
-    public static void setConceptList(HashMap<String, HashMap<String, Set<String>>> concepts) {
-        conceptList = concepts;
-    }
-
-    public static HashMap<String, Set<String>> getProperties(String type) {
+    public static HashMap<String, Set<String>> getProperties(String type, HashMap<String, HashMap<String, Set<String>>> conceptList) {
         HashMap<String, Set<String>> properties = new HashMap<>();
 
         for (Map.Entry<String, HashMap<String, Set<String>>> entry : conceptList.entrySet()) {
@@ -29,4 +23,56 @@ public class DescriptionLogicDisplayService {
 
         return properties;
     }
+
+    public static HashMap<String, HashMap<String, Set<String>>> getDataByIngredientName(Set<String> ingredients, HashMap<String, HashMap<String, HashMap<String, Set<String>>>> concepts) {
+        HashMap<String, HashMap<String, Set<String>>> dataWithName = new HashMap<>();
+
+        for (Map.Entry<String, HashMap<String, HashMap<String, Set<String>>>> outerEntry : concepts.entrySet()) {
+            String category = outerEntry.getKey();
+            HashMap<String, HashMap<String, Set<String>>> middleMap = outerEntry.getValue();
+
+            for (Map.Entry<String, HashMap<String, Set<String>>> middleEntry : middleMap.entrySet()) {
+                String conceptName = middleEntry.getKey();
+                HashMap<String, Set<String>> innerMap = middleEntry.getValue();
+
+                for (String ingredient : ingredients) {
+                    HashMap<String, Set<String>> prop = new HashMap<>();
+
+                    if (conceptName.equals(ingredient)) {
+                        prop.put("isInCategory", Collections.singleton(category));
+                        for (Map.Entry<String, Set<String>> entry : innerMap.entrySet()) {
+                            String type = entry.getKey();
+                            Set<String> propSet = entry.getValue();
+
+                            prop.put(type, propSet);
+                        }
+
+                        dataWithName.put(ingredient, prop);
+                    }
+                }
+
+            }
+        }
+
+//        System.out.println(dataWithName);
+        return dataWithName;
+    }
+
+    public static HashMap<String, Set<String>> getPropsByIngredientName(String type, HashMap<String, HashMap<String, Set<String>>> dataWithName) {
+        HashMap<String, Set<String>> properties = new HashMap<>();
+
+        for (Map.Entry<String, HashMap<String, Set<String>>> entry : dataWithName.entrySet()) {
+            String conceptName = entry.getKey();
+            HashMap<String, Set<String>> propertyMap = entry.getValue();
+
+            if (propertyMap.containsKey(type)) {
+                Set<String> propSet = propertyMap.get(type);
+
+                properties.put(conceptName, propSet);
+            }
+        }
+
+        return properties;
+    }
+
 }
