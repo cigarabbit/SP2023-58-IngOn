7d582d1e76c0956fd3dbd244b2aa5a0c8e8ebc51
runchana <runchana.see@outlook.com>
2024-04-09 15:46:21 +0700

    customize color to display on comparison page

--- IngredientSubstitution/src/main/resources/static/javascript/ingredientCustomization.js
+++ IngredientSubstitution/src/main/resources/static/javascript/ingredientCustomization.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function () {
     initializeProperties(); // Property Filter
     sortTopics();
+    displayIngredientColor();
 });
 
 var currentSortOrder = 'asc';
@@ -9,21 +10,23 @@ var foodGroup, colorProp, flavorProp, mineralProp, nutriProp, sugarProp, vitaPro
 function initializeProperties() {
     let allCategories = document.querySelectorAll('.category');
 
-    allCategories.forEach((category, index) => {
-        let checkboxes = category.querySelectorAll('.checkbox-container');
-        if (checkboxes.length > 10) {
-            for (let i = 10; i < checkboxes.length; i++) {
-                checkboxes[i].style.display = 'none';
+    if (allCategories) {
+        allCategories.forEach((category, index) => {
+            let checkboxes = category.querySelectorAll('.checkbox-container');
+            if (checkboxes.length > 10) {
+                for (let i = 10; i < checkboxes.length; i++) {
+                    checkboxes[i].style.display = 'none';
+                }
+
+                let viewMoreBtn = document.createElement('button');
+                viewMoreBtn.textContent = 'View More';
+                viewMoreBtn.setAttribute('onclick', `showMoreCheckboxes(${index})`);
+                viewMoreBtn.classList.add('view-more-btn');
+                category.appendChild(viewMoreBtn);
             }
 
-            let viewMoreBtn = document.createElement('button');
-            viewMoreBtn.textContent = 'View More';
-            viewMoreBtn.setAttribute('onclick', `showMoreCheckboxes(${index})`);
-            viewMoreBtn.classList.add('view-more-btn');
-            category.appendChild(viewMoreBtn);
-        }
-
-    })
+        })
+    }
 }
 
 function showMoreCheckboxes(categoryIndex) {
@@ -51,34 +54,101 @@ function hideCheckbox(category) {
 }
 
 function sortTopics() {
-    var sidebar = document.getElementById('sidebar-ingredient');
-    var list = sidebar.querySelector('ul');
-    var items = Array.from(list.getElementsByTagName('li'));
+    let sidebar = document.getElementById('sidebar-ingredient');
 
-    items.sort(function (a, b) {
-        const nameA = a.textContent.replace(/\(.*?\)/g, '').trim();
-        const nameB = b.textContent.replace(/\(.*?\)/g, '').trim();
-        if (currentSortOrder === 'asc') {
-            return nameA.localeCompare(nameB);
-        } else {
-            return nameB.localeCompare(nameA);
-        }
-    });
+    if (sidebar) {
+        let list = sidebar.querySelector('ul');
+        let items = Array.from(list.getElementsByTagName('li'));
 
-    list.innerHTML = "";
+        items.sort(function (a, b) {
+            const nameA = a.textContent.replace(/\(.*?\)/g, '').trim();
+            const nameB = b.textContent.replace(/\(.*?\)/g, '').trim();
+            if (currentSortOrder === 'asc') {
+                return nameA.localeCompare(nameB);
+            } else {
+                return nameB.localeCompare(nameA);
+            }
+        });
 
-    items.forEach(function (item) {
-        const itemName = item.textContent.replace(/\(.*?\)/g, '').trim();
-        if (beneProp[itemName] !== undefined) { // Not a subclass
-            list.appendChild(item);
-        }
-    });
+        list.innerHTML = "";
 
-    currentSortOrder = currentSortOrder === 'asc' ? 'desc' : 'asc';
+        items.forEach(function (item) {
+            const itemName = item.textContent.replace(/\(.*?\)/g, '').trim();
+            if (beneProp[itemName] !== undefined) { // Not a subclass
+                list.appendChild(item);
+            }
+        });
 
+        currentSortOrder = currentSortOrder === 'asc' ? 'desc' : 'asc';
+    }
 }
 
 function filterProperty() {
 
+
+}
+
+function assignColor(colorSelector, type) {
+    let colorRegex = /\w+/g;
+    let colors = colorSelector.innerHTML.match(colorRegex);
+
+    if (colors) {
+        let container;
+        if (type === 'query') {
+            container = document.querySelector('.color-box.queryBox');
+        } else {
+            container = document.querySelector('.color-box.selectedBox');
+        }
+
+        for (let i = 0; i < colors.length; i++) {
+            let circle_bg = document.createElement('div');
+            circle_bg.classList.add('color-circle');
+
+            if (colors[i] === 'Green') {
+                circle_bg.style.backgroundColor = '#49d354';
+            } else if (colors[i] === "Red") {
+                circle_bg.style.backgroundColor = '#a90e24';
+            } else if (colors[i] === "Beige") {
+                circle_bg.style.backgroundColor = '#dedec3';
+            } else if (colors[i] === "Black") {
+                circle_bg.style.backgroundColor = '#000000';
+            } else if (colors[i] === "Brown") {
+                circle_bg.style.backgroundColor = '#56350e';
+            } else if (colors[i] === "Cream") {
+                circle_bg.style.backgroundColor = '#ded4c3';
+            } else if (colors[i] === "Gold") {
+                circle_bg.style.backgroundColor = '#b28d1d';
+            } else if (colors[i] === "Gray") {
+                circle_bg.style.backgroundColor = '#808080';
+            } else if (colors[i] === "Pink") {
+                circle_bg.style.backgroundColor = '#FFB6C1';
+            } else if (colors[i] === "Purple") {
+                circle_bg.style.backgroundColor = '#800080';
+            } else if (colors[i] === "RedBrown") {
+                circle_bg.style.backgroundColor = '#A52A2A';
+            } else if (colors[i] === "Silver") {
+                circle_bg.style.backgroundColor = '#C0C0C0';
+            } else if (colors[i] === "Orange") {
+                circle_bg.style.backgroundColor = '#ff6900';
+            } else if (colors[i] === "White" || colors[i] === "Transparent") {
+                circle_bg.style.backgroundColor = '#FFFFFF';
+            } else if (colors[i] === "Yellow") {
+                circle_bg.style.backgroundColor = '#FFFF00';
+            }
+
+            container.appendChild(circle_bg);
+        }
+    }
+}
+
+
+function displayIngredientColor() {
+    let color_query = document.querySelector('#colorQuery');
+    let color_selected = document.querySelector('#colorSelected');
+
+    if (color_query && color_selected) {
+        assignColor(color_query, 'query');
+        assignColor(color_selected, 'selected');
+    }
 
 }
