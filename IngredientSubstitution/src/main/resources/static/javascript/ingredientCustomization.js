69e66511a9fc51b75c922b98352dcbcf23cb9214
runchana <runchana.see@outlook.com>
2024-04-11 23:34:46 +0700

    Update ingredientCustomization.js

--- IngredientSubstitution/src/main/resources/static/javascript/ingredientCustomization.js
+++ IngredientSubstitution/src/main/resources/static/javascript/ingredientCustomization.js
@@ -1,57 +1,10 @@
 window.addEventListener('load', function () {
-    initializeProperties(); // Property Filter
     sortTopics();
     displayIngredientColor();
 });
 
 var currentSortOrder = 'asc';
 var foodGroup, colorProp, flavorProp, mineralProp, nutriProp, sugarProp, vitaProp, shapeProp, textureProp, otherNames, typeProp;
-
-function initializeProperties() {
-    let allCategories = document.querySelectorAll('.category');
-
-    if (allCategories) {
-        allCategories.forEach((category, index) => {
-            let checkboxes = category.querySelectorAll('.checkbox-container');
-            if (checkboxes.length > 10) {
-                for (let i = 10; i < checkboxes.length; i++) {
-                    checkboxes[i].style.display = 'none';
-                }
-
-                let viewMoreBtn = document.createElement('button');
-                viewMoreBtn.textContent = 'View More';
-                viewMoreBtn.setAttribute('onclick', `showMoreCheckboxes(${index})`);
-                viewMoreBtn.classList.add('view-more-btn');
-                category.appendChild(viewMoreBtn);
-            }
-
-        })
-    }
-}
-
-function showMoreCheckboxes(categoryIndex) {
-    let allCategories = document.querySelectorAll('.category');
-    allCategories.forEach((category, index) => {
-        let checkboxes = category.querySelectorAll('.checkbox-container');
-        if (index === categoryIndex) {
-            for (let i = 0; i < checkboxes.length; i++) {
-                checkboxes[i].style.display = 'block';
-            }
-
-            category.querySelector('.view-more-btn').style.display = 'none';
-        } else {
-            for (let i = 10; i < checkboxes.length; i++) {
-                checkboxes[i].style.display = 'none';
-            }
-        }
-    });
-}
-
-
-function hideCheckbox(category) {
-    let checkboxContainer = document.querySelector('.' + category + '-container');
-    checkboxContainer.classList.toggle('hidden');
-}
 
 function sortTopics() {
     let sidebar = document.getElementById('sidebar-ingredient');
